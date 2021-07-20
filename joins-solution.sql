571567f73eb8114e6833bde8ad7d410bc66206d0
Kashia Moua <kserenitym@gmail.com>
2021-07-20 10:59:20 -0500

    changed format

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,1 +1,29 @@
+--Tasks
 
+-- 1. Get all customers and their addresses.
+
+-- 2. Get all orders and their line items (orders, quantity and product).
+
+-- 3. Which warehouses have cheetos?
+
+
+-- 4. Which warehouses have diet pepsi?
+
+-- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+
+
+-- 6. How many customers do we have?
+
+
+-- 7. How many products do we carry?
+
+-- 8. What is the total available on-hand quantity of diet pepsi?
+
+
+--Stretch
+-- 1. How much was the total cost for each order?
+
+-- 2. How much has each customer spent in total?
+
+
+-- 3. How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
